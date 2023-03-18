d2a5f790119f1bee7874017f0011d9e15eb6bc5f
Reo Hakuta <reonaldhkt+webs@gmail.com>
2023-03-18 23:48:51 +0900

    feat: ✨ (env) 環境変数の例とパーサを追加した。

--- apps/bot/src/env.ts
+++ apps/bot/src/env.ts
@@ -5,6 +5,13 @@ import { z, parseEnv } from 'znv';
 
 dotenv.config(); // Loads .env file
 
-export const { DISCORD_BOT_TOKEN } = parseEnv(process.env, {
-  DISCORD_BOT_TOKEN: z.string().min(1),
-});
+export const { NODE_ENV, DISCORD_GUILD_ID, DISCORD_BOT_TOKEN, NOTION_SECRET, NOTION_DATABASE_ID } = parseEnv(
+  process.env,
+  {
+    NODE_ENV: z.enum(['development', 'production', 'test']).default('development'),
+    DISCORD_BOT_TOKEN: z.string().min(1),
+    NOTION_SECRET: z.string().min(1),
+    NOTION_DATABASE_ID: z.string().min(1),
+    DISCORD_GUILD_ID: z.array(z.string().min(1)),
+  },
+);
